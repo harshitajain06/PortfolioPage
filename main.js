1873cba8ee12b64e1755c5c40800ad8b7214329e
harshitajain06 <harshitaforever06@gmail.com>
2022-11-16 18:04:24 +0530

    mobile changes

--- main.js
+++ main.js
@@ -2,22 +2,14 @@ const hamMenu = document.getElementById('hamMenuImg');
 const closeMenu = document.getElementById('closeMenuImg');
 const respMenu = document.getElementById('responsiveMenu');
 
-hamMenu.addEventListener('click', () => {
-  respMenu.style.display = 'flex';
-  document.querySelector('body').style.position = 'fixed';
-});
-
 function toggle() {
-    respMenu.style.display = "flex";
+  respMenu.style.display = 'flex';
 }
 
 function close1() {
-    respMenu.style.display = "none";
+  respMenu.style.display = 'none';
 }
 
 hamMenu.addEventListener('click', toggle);
 closeMenu.addEventListener('click', close1);
 respMenu.addEventListener('click', close1);
-
-
-
