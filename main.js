f8b812794cc5b8e3e5e9e815fb5bd80b738ea852
iamhassan <hassan.elouardy06@gmail.com>
2022-11-16 10:37:13 +0100

    Add changes to html css and js

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-const hamMenu = document.getElementById("hamMenuImg");
-const closeMenu = document.getElementById("closeMenuImg");
-const respMenu = document.getElementById("responsiveMenu");
+const hamMenu = document.getElementById('hamMenuImg');
+const closeMenu = document.getElementById('closeMenuImg');
+const respMenu = document.getElementById('responsiveMenu');
 
+hamMenu.addEventListener('click', () => {
+  respMenu.style.display = 'flex';
+  document.querySelector('body').style.position = 'fixed';
+});
 
-function toggle() {
-    respMenu.style.display = "flex";
-}
+closeMenu.addEventListener('click', () => {
+  respMenu.style.display = 'none';
+  document.querySelector('body').style.position = null;
+});
 
-function close1() {
-    respMenu.style.display = "none";
-}
-
-hamMenu.addEventListener('click', toggle);
-closeMenu.addEventListener('click', close1);
-respMenu.addEventListener('click', close1);
-
-
-
+respMenu.addEventListener('click', () => {
+  respMenu.style.display = 'none';
+});
