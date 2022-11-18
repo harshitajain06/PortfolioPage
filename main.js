1a0936592101e2bb1f0dd475636f7b285a10be1c
harshitajain06 <harshitaforever06@gmail.com>
2022-11-18 17:48:48 +0530

    email validation

--- main.js
+++ main.js
@@ -124,3 +124,18 @@ open.forEach((item) => {
     });
   });
 });
+
+const form = document.getElementById('form');
+const input = document.getElementById('email');
+const errorMsg = document.getElementById('errMsg');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = input.value.trim();
+  const regex = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+  if (!regex.test(email)) {
+    errorMsg.textContent = 'Email Should Be In Lower Case Only';
+  } else {
+    form.submit();
+  }
+});
